fc305fc27b517779a055bf9efc93cb3f71524bf4
Atik <atik.rucse@gmail.com>
2021-04-01 23:29:18 +0600

    Map array element and Dynamically insert into Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Razzak', 'Josim', 'Alomgir', 'Salman', 'Shuvo']
+  const productsDetails = [
+    {name: 'Pen', price: 10},
+    {name: 'Book', price: 100}
+  ] 
   const personName = [' Sohel', ' Aziz', ' Mazedul']
   const profession = [' Job/Business', ' Business', ' Business']
   var name = {
@@ -13,6 +18,9 @@ function App() {
     backgroundColor: 'cyan',
     color: 'black',
   }
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +28,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+        </ul>
+        
+          {
+            nayoks.map(nayok => <Person name={nayok}></Person> )
+          }
+        
         <Person name=" Atik" profession=" Web Dev"></Person>
         <Person name= {personName[0]} profession={profession[0]}></Person>
         <Person name= {personName[1]} profession={profession[1]}></Person>
@@ -36,8 +54,8 @@ function Person(props){
   }
   return (
     <div style={personStyle}>
-        <h3>Name:{props.name}</h3>
-        <h4>Profession:{props.profession}</h4>
+        <h3>{props.name}</h3>
+        <h4>{props.profession}</h4>
     </div>
   )
 }
